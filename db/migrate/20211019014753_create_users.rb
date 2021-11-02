6451285773df7fa423875c18b90261b2ee270a49
Michael Burt <burtrico@gmail.com>
2021-11-01 21:19:46 -0500

    added ethereum to wallets

--- db/migrate/20211019014753_create_users.rb
+++ db/migrate/20211019014753_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :ens_domain
       t.string :wallet_address
+      t.decimal :ethereum
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2021_11_07_184000) do
   create_table "users", force: :cascade do |t|
     t.string "ens_domain"
     t.string "wallet_address"
+    t.decimal "ethereum"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin"
